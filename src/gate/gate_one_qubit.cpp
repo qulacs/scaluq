d0878cb9cf66c6627f26c50a6290c718d26b1fe6
Glacialte <pikutaro25@gmail.com>
2023-10-16 22:59:21 +0900

    add Pauli Y, Z

--- src/gate/gate_one_qubit.cpp
+++ src/gate/gate_one_qubit.cpp
@@ -7,3 +7,15 @@ PauliX::PauliX(UINT target) : _target(target){};
 void PauliX::update_quantum_state(StateVector& state_vector) const {
     x_gate(this->_target, state_vector);
 }
+
+PauliY::PauliY(UINT target) : _target(target){};
+
+void PauliY::update_quantum_state(StateVector& state_vector) const {
+    y_gate(this->_target, state_vector);
+}
+
+PauliZ::PauliZ(UINT target) : _target(target){};
+
+void PauliZ::update_quantum_state(StateVector& state_vector) const {
+    z_gate(this->_target, state_vector);
+}
--- src/gate/update_ops_one_qubit.cpp
+++ src/gate/update_ops_one_qubit.cpp
@@ -15,10 +15,27 @@ void x_gate(UINT target_qubit_index, StateVector& state) {
         });
 }
 
-void y_gate(UINT target_qubit_index, StateVector& state) {}
+void y_gate(UINT target_qubit_index, StateVector& state) {
+    const UINT n_qubits = state.n_qubits();
+    const UINT low_mask = (1ULL << target_qubit_index) - 1;
+    const UINT high_mask = ~low_mask;
+    const Complex im(0, 1);
+    Kokkos::parallel_for(
+        1ULL << (n_qubits - 1), KOKKOS_LAMBDA(const UINT& it) {
+            UINT i = (it & high_mask) << 1 | (it & low_mask);
+            state[i] *= -im;
+            state[i | (1ULL << target_qubit_index)] *= im;
+            Kokkos::Experimental::swap(state[i], state[i | (1ULL << target_qubit_index)]);
+        });
+}
 
 void z_gate(UINT target_qubit_index, StateVector& state) {
     const UINT n_qubits = state.n_qubits();
     const UINT low_mask = (1ULL << target_qubit_index) - 1;
     const UINT high_mask = ~low_mask;
+    Kokkos::parallel_for(
+        1ULL << (n_qubits - 1), KOKKOS_LAMBDA(const UINT& it) {
+            UINT i = (it & high_mask) << 1 | (it & low_mask);
+            state[i | (1ULL << target_qubit_index)] *= -1;
+        });
 }
