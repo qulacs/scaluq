46db5e822871a5632e13d29a986fa74d7b3520fd
Koki Kawamura <kowerkoint@gmail.com>
2023-10-17 09:57:40 +0900

    add StateVector::amplitudes_raw

--- src/gate/update_ops_one_qubit.cpp
+++ src/gate/update_ops_one_qubit.cpp
@@ -6,11 +6,12 @@
 
 void x_gate(UINT target_qubit_index, StateVector& state) {
     const UINT n_qubits = state.n_qubits();
+    auto amplitudes = state.amplitudes_raw();
     const UINT low_mask = (1ULL << target_qubit_index) - 1;
     const UINT high_mask = ~low_mask;
     Kokkos::parallel_for(
         1ULL << (n_qubits - 1), KOKKOS_LAMBDA(const UINT& it) {
             UINT i = (it & high_mask) << 1 | (it & low_mask);
-            Kokkos::Experimental::swap(state[i], state[i | (1ULL << target_qubit_index)]);
+            Kokkos::Experimental::swap(amplitudes[i], amplitudes[i | (1ULL << target_qubit_index)]);
         });
 }
--- src/state/state_vector.cpp
+++ src/state/state_vector.cpp
@@ -6,3 +6,6 @@ StateVector::StateVector(UINT n_qubits)
       _amplitudes(Kokkos::View<Complex*>("state", this->_dim)) {
     this->_amplitudes[0] = 1.0 + 0.0i;
 }
+
+Kokkos::View<Complex*>& StateVector::amplitudes_raw() { return this->_amplitudes; }
+const Kokkos::View<Complex*>& StateVector::amplitudes_raw() const { return this->_amplitudes; }
