714c9c6aba3f7bcf869e6af9d2dce8784468789b
Glacialte <pikutaro25@gmail.com>
2023-10-26 17:34:48 +0900

    fix compute_squared_norm, normalize

--- qulacs/state/state_vector.cpp
+++ qulacs/state/state_vector.cpp
@@ -8,7 +8,7 @@ StateVector::StateVector(UINT n_qubits)
     this->_amplitudes[0] = 1.0 + 0.0i;
 }
 
-static StateVector StateVector::Haar_random_state(UINT n_qubits) const {
+static StateVector StateVector::Haar_random_state(UINT n_qubits) {
     StateVector state_vector(n_qubits);
     Random rng;
     for (int i = 0; i < state_vector.dim(); i++) {
@@ -26,7 +26,7 @@ Kokkos::View<Complex*>& StateVector::amplitudes_raw() { return this->_amplitudes
 
 const Kokkos::View<Complex*>& StateVector::amplitudes_raw() const { return this->_amplitudes; }
 
-const std::vector<Complex>& amplitudes() const { return this->_amplitudes; }
+const std::vector<Complex>& StateVector::amplitudes() const { return this->_amplitudes; }
 
 Complex& StateVector::operator[](const int index) & { return this->_amplitudes[index]; }
 
@@ -36,7 +36,8 @@ const Complex& StateVector::operator[](const int index) const& {
 
 double StateVector::compute_squared_norm() const {
     double norm = 0.;
-    for (const auto& amplitude : this->_amplitudes) {
+    std::vector<Complex>& amplitudes = state.amplitudes();
+    for (const auto& amplitude : amplitudes) {
         norm += std::norm(amplitude);
     }
     return norm;
@@ -44,7 +45,8 @@ double StateVector::compute_squared_norm() const {
 
 void StateVector::normalize() {
     const auto norm = std::sqrt(this->compute_squared_norm());
-    for (auto& amplitude : this->_amplitudes) {
+    std::vector<Complex>& amplitudes = state.amplitudes();
+    for (auto& amplitude : amplitudes) {
         amplitude /= norm;
     }
 }
