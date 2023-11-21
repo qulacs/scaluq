86314b6fee129c6866e71f1426d6f31d052e3f79
gandalfr-KY <suraimuempera@gmail.com>
2023-11-21 02:59:02 +0000

    setup of build environment for GPU

--- qulacs/types.hpp
+++ qulacs/types.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <Kokkos_Core.hpp>
 #include <complex>
 #include <cstdint>
 
 namespace qulacs {
 using UINT = std::uint64_t;
 
-using Complex = std::complex<double>;
+using Complex = Kokkos::complex<double>;
 using namespace std::complex_literals;
 }  // namespace qulacs
