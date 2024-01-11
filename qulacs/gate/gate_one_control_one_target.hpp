5e6d67d7db07488dadcd7b782eb597a0dca0ceb3
KowerKoint <kowerkoint@gmail.com>
2024-01-11 19:04:22 +0900

    GatePtr, gate_factory

--- qulacs/gate/gate_one_control_one_target.hpp
+++ qulacs/gate/gate_one_control_one_target.hpp
@@ -5,19 +5,38 @@
 #include "gate.hpp"
 
 namespace qulacs {
-class CNOT : public QuantumGate {
+class QuantumGateOneControlOneTarget : public QuantumGate {
+protected:
     UINT _control, _target;
 
 public:
-    CNOT(UINT control, UINT target) : _control(control), _target(target){};
+    QuantumGateOneControlOneTarget(UINT control, UINT target)
+        : _control(control), _target(target){};
+
+    UINT control() const { return _control; }
+    UINT target() const { return _target; }
+
+    std::vector<UINT> get_target_qubit_list() const override { return {_target}; }
+    std::vector<UINT> get_control_qubit_list() const override { return {_control}; };
+};
+
+class CNOT : public QuantumGateOneControlOneTarget {
+public:
+    CNOT(UINT control, UINT target) : QuantumGateOneControlOneTarget(control, target) {}
+
+    GatePtr copy() const override { return std::make_unique<CNOT>(*this); }
+    GatePtr get_inverse() const override { return std::make_unique<CNOT>(*this); }
+
     void update_quantum_state(StateVector& state_vector) const override;
 };
 
-class CZ : public QuantumGate {
-    UINT _control, _target;
-
+class CZ : public QuantumGateOneControlOneTarget {
 public:
-    CZ(UINT control, UINT target) : _control(control), _target(target){};
+    CZ(UINT control, UINT target) : QuantumGateOneControlOneTarget(control, target) {}
+
+    GatePtr copy() const override { return std::make_unique<CZ>(*this); }
+    GatePtr get_inverse() const override { return std::make_unique<CZ>(*this); }
+
     void update_quantum_state(StateVector& state_vector) const override;
 };
 }  // namespace qulacs
