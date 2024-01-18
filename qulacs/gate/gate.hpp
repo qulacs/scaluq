ccfa33bc95ff64535ffbabfd952f85f312c6582f
KowerKoint <kowerkoint@gmail.com>
2024-01-18 16:05:51 +0900

    remove n_qubits()

--- qulacs/gate/gate.hpp
+++ qulacs/gate/gate.hpp
@@ -15,14 +15,6 @@ public:
 
     [[nodiscard]] virtual std::vector<UINT> get_target_qubit_list() const = 0;
     [[nodiscard]] virtual std::vector<UINT> get_control_qubit_list() const = 0;
-    [[nodiscard]] UINT n_qubits() const {
-        auto target_list = get_target_qubit_list();
-        auto control_list = get_control_qubit_list();
-        UINT ret = 0;
-        if (!target_list.empty()) ret = std::max(ret, *std::ranges::max_element(target_list) + 1);
-        if (!control_list.empty()) ret = std::max(ret, *std::ranges::max_element(control_list) + 1);
-        return ret;
-    }
 
     [[nodiscard]] virtual GatePtr copy() const = 0;
     [[nodiscard]] virtual GatePtr get_inverse() const = 0;
@@ -46,7 +38,6 @@ private:
 public:
     std::vector<UINT> get_control_qubit_list() const { return _gate_ptr->get_control_qubit_list(); }
     std::vector<UINT> get_target_qubit_list() const { return _gate_ptr->get_target_qubit_list(); }
-    UINT n_qubits() const { return _gate_ptr->n_qubits(); }
     Gate copy() const { return _gate_ptr->copy(); }
     Gate get_inverse() const { return _gate_ptr->get_inverse(); }
     void update_quantum_state(StateVector& state_vector) const {
