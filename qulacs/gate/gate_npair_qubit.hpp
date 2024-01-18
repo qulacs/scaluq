5dd3f4274e7e81384c292e84acfac5012f10f074
Glacialte <pikutaro25@gmail.com>
2024-01-18 07:04:10 +0000

    add runtime_error to gate_npair_qubit.hpp

--- qulacs/gate/gate_npair_qubit.hpp
+++ qulacs/gate/gate_npair_qubit.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cassert>
+
 #include "gate.hpp"
 
 namespace qulacs {
@@ -8,7 +10,14 @@ class FusedSWAP : public QuantumGate {
 
 public:
     FusedSWAP(UINT _qubit_index1, UINT _qubit_index2, UINT _block_size)
-        : qubit_index1(_qubit_index1), qubit_index2(_qubit_index2), block_size(_block_size){};
+        : qubit_index1(_qubit_index1), qubit_index2(_qubit_index2), block_size(_block_size) {
+        UINT upper_index = std::max(_qubit_index1, _qubit_index2);
+        UINT lower_index = std::min(_qubit_index1, _qubit_index2);
+        if (upper_index > (lower_index + block_size - 1)) {
+            throw std::runtime_error(
+                "FusedSwap: upper index must be bigger than lower_index + block_size - 1");
+        }
+    };
 
     void update_quantum_state(StateVector& state_vector) const override;
 };
