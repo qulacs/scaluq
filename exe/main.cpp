2c8d6a54823ef170fd2946706b82cbfaf1ca2c82
KowerKoint <kowerkoint@gmail.com>
2024-01-25 18:53:21 +0900

    restore exe/main

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
-#include "operator/operator.hpp"
+#include "state/state_vector.hpp"
 
 using namespace qulacs;
 
 void run() {
-    auto op1 = Operator(2);
-    op1.add_random_operator(2);
-    auto op2 = Operator(2);
-    op2.add_random_operator(2);
-    std::cerr << op1.to_string() << std::endl;
-    std::cerr << op2.to_string() << std::endl;
-    std::cerr << (op1 * op2).to_string() << std::endl;
+    auto state = StateVector::Haar_random_state(3);
+    for (UINT i = 0; i < state.dim(); i++) {
+        std::cout << state[i] << std::endl;
+    }
 }
 
 int main() {
