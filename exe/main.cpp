3521758e056607d5931720410c1e8dd0b5301fd8
gandalfr-KY <90887153+gandalfr-KY@users.noreply.github.com>
2024-02-21 14:00:42 +0900

    Update main.cpp

--- exe/main.cpp
+++ exe/main.cpp
@@ -7,7 +7,7 @@ using namespace qulacs;
 void run() {
     auto state = StateVector::Haar_random_state(3);
     for (UINT i = 0; i < state.dim(); i++) {
-        std::cout << state[i] << std::endl;
+        // std::cout << state[i] << std::endl;
     }
 }
 
