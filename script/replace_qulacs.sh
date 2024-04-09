adfa478b1ced27a841fff2891e469f536cb7863f
KowerKoint <kowerkoint@gmail.com>
2024-04-09 12:57:43 +0900

    deal with QULACS2023

--- script/replace_qulacs.sh
+++ script/replace_qulacs.sh
@@ -7,7 +7,7 @@ for file in $files; do
     if [ $file = $0 ]; then
         continue
     fi
-    sed -i -e 's/qulacs2023/scaluq/g' -e 's/qulacs/scaluq/g' -e 's/QULACS/SCALUQ/g' -e 's/scaluq.osaka@gmail.com/qulacs.osaka@gmail.com/g' $file
+    sed -i -e 's/qulacs2023/scaluq/g' -e 's/qulacs/scaluq/g' -e 's/QULACS2023/SCALUQ/g' -e 's/QULACS/SCALUQ/g' -e 's/scaluq.osaka@gmail.com/qulacs.osaka@gmail.com/g' $file
     if [ $file = *'qulacs'* ]; then
         echo 'Warning: filename including qulacs: ' $file
     fi
