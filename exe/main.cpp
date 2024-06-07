f407c4a5477921230e50dae7ded79cf338dec9fd
gandalfr-KY <oukanimiso@gmail.com>
2024-06-07 10:53:32 +0900

    fix serial coded

--- exe/main.cpp
+++ exe/main.cpp
@@ -11,7 +11,7 @@ using namespace scaluq;
 using namespace std;
 
 template <typename Layout>
-double run(UINT n_q, UINT batch, UINT loop = 10) {
+double run(UINT n_q, UINT batch, UINT loop = 6) {
     std::cout << "n_qubits: " << n_q << ", batch: " << batch << std::endl;
     Kokkos::Timer tm;
     tm.reset();
@@ -25,12 +25,28 @@ double run(UINT n_q, UINT batch, UINT loop = 10) {
     return tm.seconds() / loop;
 }
 
-double run_serial(UINT n_q, UINT batch, UINT loop = 10) {
+double run_serial(UINT n_q, UINT batch, UINT loop = 6) {
     std::cout << "n_qubits: " << n_q << ", batch: " << batch << std::endl;
     Kokkos::Timer tm;
     tm.reset();
     for (UINT i = 0; i < loop; ++i) {
-        std::vector<StateVector> states(batch, StateVector::Haar_random_state(n_q, 0));
+        for (UINT b = 0; b < batch; ++b) {
+            auto state = StateVector::Haar_random_state(n_q, 0);
+            auto ent = state.get_entropy();
+            auto nrm = state.get_squared_norm();
+            auto prb = state.get_zero_probability(n_q / 2);
+        }
+    }
+    return tm.seconds() / loop;
+}
+
+double run_serial_all_memory(UINT n_q, UINT batch, UINT loop = 6) {
+    std::cout << "n_qubits: " << n_q << ", batch: " << batch << std::endl;
+    Kokkos::Timer tm;
+    tm.reset();
+    for (UINT i = 0; i < loop; ++i) {
+        std::vector<StateVector> states(batch);
+        for (UINT b = 0; b < batch; ++b) states[b] = StateVector::Haar_random_state(n_q, 0);
         for (auto &state : states) auto ent = state.get_entropy();
         for (auto &state : states) auto nrm = state.get_squared_norm();
         for (auto &state : states) auto prb = state.get_zero_probability(n_q / 2);
@@ -45,6 +61,7 @@ int main() {
     std::vector left(n_lim + 1, std::vector<double>(batch_lim + 1, -1));
     std::vector right(n_lim + 1, std::vector<double>(batch_lim + 1, -1));
     std::vector serial(n_lim + 1, std::vector<double>(batch_lim + 1, -1));
+    std::vector serial_all_mem(n_lim + 1, std::vector<double>(batch_lim + 1, -1));
     for (UINT n = 1; n <= n_lim; ++n) {
         for (UINT b = 0; b <= batch_lim; ++b) {
             const UINT batch_size = [](UINT pw) {
@@ -55,6 +72,7 @@ int main() {
             left[n][b] = run<Kokkos::LayoutLeft>(n, batch_size);
             right[n][b] = run<Kokkos::LayoutRight>(n, batch_size);
             serial[n][b] = run_serial(n, batch_size);
+            serial_all_mem[n][b] = run_serial_all_memory(n, batch_size);
         }
     }
 
@@ -77,6 +95,14 @@ int main() {
     std::cout << "Serial:\n";
     for (UINT n = 1; n <= n_lim; ++n) {
         for (auto d : serial[n]) {
+            std::cout << d << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Serial(all states are on memory):\n";
+    for (UINT n = 1; n <= n_lim; ++n) {
+        for (auto d : serial_all_mem[n]) {
             std::cout << d << " ";
         }
         std::cout << std::endl;
