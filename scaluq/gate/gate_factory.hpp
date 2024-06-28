33a59f922c788e40117f440c0194c6cdef2edb13
KowerKoint <kowerkoint@gmail.com>
2024-06-28 18:11:13 +0900

    WIP: gate_factory for DenseMatrix

--- scaluq/gate/gate_factory.hpp
+++ scaluq/gate/gate_factory.hpp
@@ -83,6 +83,9 @@ inline Gate U2(UINT target, double phi, double lambda) {
 inline Gate U3(UINT target, double theta, double phi, double lambda) {
     return internal::GateFactory::create_gate<internal::U3GateImpl>(target, theta, phi, lambda);
 }
+inline Gate DenseMatrix(UINT target, const std::array<std::array<Complex, 2>, 2>& matrix) {
+    return internal::GateFactory::create_gate<internal::OneQubitMatrixGateImpl>(target, matrix);
+}
 inline Gate CX(UINT control, UINT target) {
     return internal::GateFactory::create_gate<internal::CXGateImpl>(control, target);
 }
@@ -97,11 +100,50 @@ inline Gate FusedSwap(UINT qubit_index1, UINT qubit_index2, UINT block_size) {
     return internal::GateFactory::create_gate<internal::FusedSwapGateImpl>(
         qubit_index1, qubit_index2, block_size);
 }
+inline Gate DenseMatrix(UINT target1,
+                        UINT target2,
+                        const std::array<std::array<Complex, 4>, 4>& matrix) {
+    return internal::GateFactory::create_gate<internal::TwoQubitMatrixGateImpl>(
+        target1, target2, matrix);
+}
 inline Gate Pauli(const PauliOperator& pauli) {
     return internal::GateFactory::create_gate<internal::PauliGateImpl>(pauli);
 }
 inline Gate PauliRotation(const PauliOperator& pauli, double angle) {
     return internal::GateFactory::create_gate<internal::PauliRotationGateImpl>(pauli, angle);
+}
+inline Gate DenseMatrix(const std::vector<UINT>& targets, const ComplexMatrix& matrix) {
+    UINT nqubits = targets.size();
+    UINT dim = 1ULL << nqubits;
+    if (matrix.rows() != dim || matrix.cols() != dim) {
+        throw std::runtime_error(
+            "gate::DenseMatrix(const std::vector<UINT>&, const ComplexMatrix&): matrix size must "
+            "be 2^{n_qubits} x 2^{n_qubits}.");
+    }
+    if (targets.size() == 0) return I();
+    if (targets.size() == 1) {
+        return DenseMatrix(targets[0],
+                           std::array{std::array{Complex(matrix(0, 0)), Complex(matrix(0, 1))},
+                                      std::array{Complex(matrix(1, 0)), Complex(matrix(1, 1))}});
+    }
+    if (targets.size() == 2) {
+        return DenseMatrix(
+            targets[0],
+            targets[1],
+            std::array{
+                std::array{
+                    Complex(matrix(0, 0)), Complex(matrix(0, 1)), Complex(0, 2), Complex(0, 3)},
+                std::array{
+                    Complex(matrix(1, 0)), Complex(matrix(1, 1)), Complex(1, 2), Complex(1, 3)},
+                std::array{
+                    Complex(matrix(2, 0)), Complex(matrix(2, 1)), Complex(2, 2), Complex(2, 3)},
+                std::array{
+                    Complex(matrix(3, 0)), Complex(matrix(3, 1)), Complex(3, 2), Complex(3, 3)},
+            });
+    }
+    throw std::runtime_error(
+        "gate::DenseMatrix(const std::vector<UINT>&, const ComplexMatrix&): DenseMatrix gate more "
+        "than two qubits is not implemented yet.");
 }
 }  // namespace gate
 }  // namespace scaluq
