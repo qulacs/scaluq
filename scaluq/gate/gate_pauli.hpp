bb9417f690a94e1cb78b8097a5ded7c3685b8b2e
gandalfr-KY <oukanimiso@gmail.com>
2024-08-12 15:02:51 +0000

    add cast to std::complex

--- scaluq/gate/gate_pauli.hpp
+++ scaluq/gate/gate_pauli.hpp
@@ -49,8 +49,9 @@ public:
         ComplexMatrix mat = this->_pauli.get_matrix_ignoring_coef();
         Complex true_angle = _angle * _pauli.get_coef();
         StdComplex imag_unit(0, 1);
-        mat = Kokkos::cos(-true_angle / 2) * ComplexMatrix::Identity(mat.rows(), mat.cols()) +
-              imag_unit * Kokkos::sin(-true_angle / 2) * mat;
+        mat = (StdComplex)Kokkos::cos(-true_angle / 2) *
+                  ComplexMatrix::Identity(mat.rows(), mat.cols()) +
+              imag_unit * (StdComplex)Kokkos::sin(-true_angle / 2) * mat;
         return mat;
     }
     void update_quantum_state(StateVector& state_vector) const override {
