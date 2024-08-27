e15779603fa9de4867f15f78ea641d059bd92f5a
gandalfr-KY <oukanimiso@gmail.com>
2024-08-27 00:37:16 +0000

    UINT -> std:uint64_t

--- scaluq/gate/param_gate_factory.hpp
+++ scaluq/gate/param_gate_factory.hpp
@@ -15,22 +15,28 @@ public:
 };
 }  // namespace internal
 namespace gate {
-inline ParamGate PRX(UINT target, double pcoef = 1., const std::vector<UINT>& controls = {}) {
+inline ParamGate PRX(std::uint64_t target,
+                     double pcoef = 1.,
+                     const std::vector<std::uint64_t>& controls = {}) {
     return internal::ParamGateFactory::create_gate<internal::PRXGateImpl>(
         internal::vector_to_mask({target}), internal::vector_to_mask(controls), pcoef);
 }
-inline ParamGate PRY(UINT target, double pcoef = 1., const std::vector<UINT>& controls = {}) {
+inline ParamGate PRY(std::uint64_t target,
+                     double pcoef = 1.,
+                     const std::vector<std::uint64_t>& controls = {}) {
     return internal::ParamGateFactory::create_gate<internal::PRYGateImpl>(
         internal::vector_to_mask({target}), internal::vector_to_mask(controls), pcoef);
 }
-inline ParamGate PRZ(UINT target, double pcoef = 1., const std::vector<UINT>& controls = {}) {
+inline ParamGate PRZ(std::uint64_t target,
+                     double pcoef = 1.,
+                     const std::vector<std::uint64_t>& controls = {}) {
     return internal::ParamGateFactory::create_gate<internal::PRZGateImpl>(
         internal::vector_to_mask({target}), internal::vector_to_mask(controls), pcoef);
 }
 // まだ
 inline ParamGate PPauliRotation(const PauliOperator& pauli,
                                 double pcoef = 1.,
-                                const std::vector<UINT>& controls = {}) {
+                                const std::vector<std::uint64_t>& controls = {}) {
     return internal::ParamGateFactory::create_gate<internal::PPauliRotationGateImpl>(
         internal::vector_to_mask(controls), pauli, pcoef);
 }
--- scaluq/state/state_vector_batched.hpp
+++ scaluq/state/state_vector_batched.hpp
@@ -5,39 +5,40 @@
 
 namespace scaluq {
 class StateVectorBatched {
-    UINT _batch_size;
-    UINT _n_qubits;
-    UINT _dim;
+    std::uint64_t _batch_size;
+    std::uint64_t _n_qubits;
+    std::uint64_t _dim;
 
 public:
     StateVectorBatchedView _raw;
     StateVectorBatched() = default;
-    StateVectorBatched(UINT batch_size, UINT n_qubits);
+    StateVectorBatched(std::uint64_t batch_size, std::uint64_t n_qubits);
     StateVectorBatched(const StateVectorBatched& other) = default;
 
     StateVectorBatched& operator=(const StateVectorBatched& other) = default;
 
-    [[nodiscard]] UINT n_qubits() const { return this->_n_qubits; }
+    [[nodiscard]] std::uint64_t n_qubits() const { return this->_n_qubits; }
 
-    [[nodiscard]] UINT dim() const { return this->_dim; }
+    [[nodiscard]] std::uint64_t dim() const { return this->_dim; }
 
-    [[nodiscard]] UINT batch_size() const { return this->_batch_size; }
+    [[nodiscard]] std::uint64_t batch_size() const { return this->_batch_size; }
 
     void set_state_vector(const StateVector& state);
-    void set_state_vector(UINT batch_id, const StateVector& state);
-    [[nodiscard]] StateVector get_state_vector(UINT batch_id) const;
+    void set_state_vector(std::uint64_t batch_id, const StateVector& state);
+    [[nodiscard]] StateVector get_state_vector(std::uint64_t batch_id) const;
 
     void set_zero_state();
-    void set_computational_basis(UINT basis);
+    void set_computational_basis(std::uint64_t basis);
     void set_zero_norm_state();
 
-    [[nodiscard]] std::vector<std::vector<UINT>> sampling(UINT sampling_count,
-                                                          UINT seed = std::random_device()()) const;
+    [[nodiscard]] std::vector<std::vector<std::uint64_t>> sampling(
+        std::uint64_t sampling_count, std::uint64_t seed = std::random_device()()) const;
 
-    [[nodiscard]] static StateVectorBatched Haar_random_states(UINT batch_size,
-                                                               UINT n_qubits,
-                                                               bool set_same_state,
-                                                               UINT seed = std::random_device()());
+    [[nodiscard]] static StateVectorBatched Haar_random_states(
+        std::uint64_t batch_size,
+        std::uint64_t n_qubits,
+        bool set_same_state,
+        std::uint64_t seed = std::random_device()());
 
     [[nodiscard]] std::vector<std::vector<Complex>> amplitudes() const;
 
@@ -45,9 +46,9 @@ public:
 
     void normalize();
 
-    [[nodiscard]] std::vector<double> get_zero_probability(UINT target_qubit_index) const;
+    [[nodiscard]] std::vector<double> get_zero_probability(std::uint64_t target_qubit_index) const;
     [[nodiscard]] std::vector<double> get_marginal_probability(
-        const std::vector<UINT>& measured_values) const;
+        const std::vector<std::uint64_t>& measured_values) const;
     [[nodiscard]] std::vector<double> get_entropy() const;
 
     void add_state_vector(const StateVectorBatched& states);
