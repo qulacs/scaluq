1bcd981dcc276549bb3e178fd5984a34b4ce22ba
gandalfr-KY <oukanimiso@gmail.com>
2024-08-30 03:31:02 +0000

    .

--- exe/main.cpp
+++ exe/main.cpp
@@ -14,15 +14,7 @@ using namespace std;
 
 void run() {
     std::uint64_t n_qubits = 5;
-    Kokkos::parallel_for(
-        10, KOKKOS_LAMBDA(int i) {
-            Kokkos::printf("{%lf, %lf, %lf, %lf}\n",
-                           EXAMPLE(0).real(),
-                           EXAMPLE(1).real(),
-                           EXAMPLE(2).real(),
-                           EXAMPLE(3).real());
-            Kokkos::printf("%lf\n", INVERSE_SQRT2);
-        });
+    auto state = StateVector::Haar_random_state(n_qubits);
 }
 
 int main() {
