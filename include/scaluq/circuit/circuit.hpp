82df9d9905fdef44c3c1a95fa3aa690887174481
KowerKoint <kowerkoint@gmail.com>
2024-11-15 11:13:29 +0900

    transfer Circuit

--- include/scaluq/circuit/circuit.hpp
+++ include/scaluq/circuit/circuit.hpp
@@ -18,13 +18,7 @@ public:
     [[nodiscard]] inline std::uint64_t n_qubits() const { return _n_qubits; }
     [[nodiscard]] inline const std::vector<GateWithKey>& gate_list() const { return _gate_list; }
     [[nodiscard]] inline std::uint64_t n_gates() { return _gate_list.size(); }
-    [[nodiscard]] inline const std::set<std::string> key_set() const {
-        std::set<std::string> key_set;
-        for (auto&& gate : _gate_list) {
-            if (gate.index() == 1) key_set.insert(std::get<1>(gate).second);
-        }
-        return key_set;
-    }
+    [[nodiscard]] std::set<std::string> key_set() const;
     [[nodiscard]] inline const GateWithKey& get_gate_at(std::uint64_t idx) const {
         if (idx >= _gate_list.size()) {
             throw std::runtime_error("Circuit::get_gate_at(std::uint64_t): index out of bounds");
@@ -41,35 +35,7 @@ public:
         return std::get<1>(gate).second;
     }
 
-    [[nodiscard]] std::uint64_t calculate_depth() const {
-        std::vector<std::uint64_t> filled_step(_n_qubits, 0ULL);
-        for (const auto& gate : _gate_list) {
-            std::vector<std::uint64_t> control_qubits =
-                gate.index() == 0 ? std::get<0>(gate)->control_qubit_list()
-                                  : std::get<1>(gate).first->control_qubit_list();
-            std::vector<std::uint64_t> target_qubits =
-                gate.index() == 0 ? std::get<0>(gate)->target_qubit_list()
-                                  : std::get<1>(gate).first->target_qubit_list();
-            std::uint64_t max_step_amount_target_qubits = 0;
-            for (std::uint64_t control : control_qubits) {
-                if (max_step_amount_target_qubits < filled_step[control]) {
-                    max_step_amount_target_qubits = filled_step[control];
-                }
-            }
-            for (std::uint64_t target : control_qubits) {
-                if (max_step_amount_target_qubits < filled_step[target]) {
-                    max_step_amount_target_qubits = filled_step[target];
-                }
-            }
-            for (std::uint64_t control : control_qubits) {
-                filled_step[control] = max_step_amount_target_qubits + 1;
-            }
-            for (std::uint64_t target : target_qubits) {
-                filled_step[target] = max_step_amount_target_qubits + 1;
-            }
-        }
-        return *std::ranges::max_element(filled_step);
-    }
+    [[nodiscard]] std::uint64_t calculate_depth() const;
 
     void add_gate(const Gate<Fp>& gate) {
         check_gate_is_valid(gate);
@@ -88,110 +54,24 @@ public:
         _gate_list.push_back(std::make_pair(std::move(param_gate), std::string(parameter_key)));
     }
 
-    void add_circuit(const Circuit<Fp>& circuit) {
-        if (circuit._n_qubits != _n_qubits) {
-            throw std::runtime_error(
-                "Circuit::add_circuit(const Circuit&): circuit with different qubit count cannot "
-                "be "
-                "merged.");
-        }
-        _gate_list.reserve(_gate_list.size() + circuit._gate_list.size());
-        for (const auto& gate : circuit._gate_list) {
-            _gate_list.push_back(gate);
-        }
-    }
-    void add_circuit(Circuit<Fp>&& circuit) {
-        if (circuit._n_qubits != _n_qubits) {
-            throw std::runtime_error(
-                "Circuit::add_circuit(Circuit&&): circuit with different qubit count cannot be "
-                "merged.");
-        }
-        _gate_list.reserve(_gate_list.size() + circuit._gate_list.size());
-        for (auto&& gate : circuit._gate_list) {
-            _gate_list.push_back(std::move(gate));
-        }
-    }
+    void add_circuit(const Circuit<Fp>& circuit);
+    void add_circuit(Circuit<Fp>&& circuit);
 
     void update_quantum_state(StateVector<Fp>& state,
-                              const std::map<std::string, Fp>& parameters = {}) const {
-        for (auto&& gate : _gate_list) {
-            if (gate.index() == 0) continue;
-            const auto& key = std::get<1>(gate).second;
-            if (!parameters.contains(key)) {
-                using namespace std::string_literals;
-                throw std::runtime_error(
-                    "Circuit::update_quantum_state(StateVector&, const std::map<std::string_view, double>&) const: parameter named "s +
-                    std::string(key) + "is not given.");
-            }
-        }
-        for (auto&& gate : _gate_list) {
-            if (gate.index() == 0) {
-                std::get<0>(gate)->update_quantum_state(state);
-            } else {
-                const auto& [param_gate, key] = std::get<1>(gate);
-                param_gate->update_quantum_state(state, parameters.at(key));
-            }
-        }
-    }
+                              const std::map<std::string, Fp>& parameters = {}) const;
 
-    Circuit copy() const {
-        Circuit ccircuit(_n_qubits);
-        ccircuit._gate_list.reserve(_gate_list.size());
-        for (auto&& gate : _gate_list) {
-            if (gate.index() == 0)
-                ccircuit._gate_list.push_back(std::get<0>(gate));
-            else {
-                const auto& [param_gate, key] = std::get<1>(gate);
-                ccircuit._gate_list.push_back(std::make_pair(param_gate, key));
-            }
-        }
-        return ccircuit;
-    }
+    Circuit copy() const;
 
-    Circuit get_inverse() const {
-        Circuit icircuit(_n_qubits);
-        icircuit._gate_list.reserve(_gate_list.size());
-        for (auto&& gate : _gate_list | std::views::reverse) {
-            if (gate.index() == 0)
-                icircuit._gate_list.push_back(std::get<0>(gate)->get_inverse());
-            else {
-                const auto& [param_gate, key] = std::get<1>(gate);
-                icircuit._gate_list.push_back(std::make_pair(param_gate->get_inverse(), key));
-            }
-        }
-        return icircuit;
-    }
+    Circuit get_inverse() const;
 
 private:
     std::uint64_t _n_qubits;
 
     std::vector<GateWithKey> _gate_list;
 
-    void check_gate_is_valid(const Gate<Fp>& gate) const {
-        auto targets = gate->target_qubit_list();
-        auto controls = gate->control_qubit_list();
-        bool valid = true;
-        if (!targets.empty())
-            valid &= *std::max_element(targets.begin(), targets.end()) < _n_qubits;
-        if (!controls.empty())
-            valid &= *std::max_element(controls.begin(), controls.end()) < _n_qubits;
-        if (!valid) {
-            throw std::runtime_error("Gate to be added to the circuit has invalid qubit range");
-        }
-    }
+    void check_gate_is_valid(const Gate<Fp>& gate) const;
 
-    void check_gate_is_valid(const ParamGate<Fp>& gate) const {
-        auto targets = gate->target_qubit_list();
-        auto controls = gate->control_qubit_list();
-        bool valid = true;
-        if (!targets.empty())
-            valid &= *std::max_element(targets.begin(), targets.end()) < _n_qubits;
-        if (!controls.empty())
-            valid &= *std::max_element(controls.begin(), controls.end()) < _n_qubits;
-        if (!valid) {
-            throw std::runtime_error("Gate to be added to the circuit has invalid qubit range");
-        }
-    }
+    void check_gate_is_valid(const ParamGate<Fp>& gate) const;
 };
 
 #ifdef SCALUQ_USE_NANOBIND
---
+++ src/circuit/circuit.cpp
@@ -0,0 +1,147 @@
+#include <scaluq/circuit/circuit.hpp>
+
+#include "../util/template.hpp"
+
+namespace scaluq {
+FLOAT(Fp)
+std::set<std::string> Circuit<Fp>::key_set() const {
+    std::set<std::string> key_set;
+    for (auto&& gate : _gate_list) {
+        if (gate.index() == 1) key_set.insert(std::get<1>(gate).second);
+    }
+    return key_set;
+}
+
+FLOAT(Fp)
+std::uint64_t Circuit<Fp>::calculate_depth() const {
+    std::vector<std::uint64_t> filled_step(_n_qubits, 0ULL);
+    for (const auto& gate : _gate_list) {
+        std::vector<std::uint64_t> control_qubits =
+            gate.index() == 0 ? std::get<0>(gate)->control_qubit_list()
+                              : std::get<1>(gate).first->control_qubit_list();
+        std::vector<std::uint64_t> target_qubits =
+            gate.index() == 0 ? std::get<0>(gate)->target_qubit_list()
+                              : std::get<1>(gate).first->target_qubit_list();
+        std::uint64_t max_step_amount_target_qubits = 0;
+        for (std::uint64_t control : control_qubits) {
+            if (max_step_amount_target_qubits < filled_step[control]) {
+                max_step_amount_target_qubits = filled_step[control];
+            }
+        }
+        for (std::uint64_t target : control_qubits) {
+            if (max_step_amount_target_qubits < filled_step[target]) {
+                max_step_amount_target_qubits = filled_step[target];
+            }
+        }
+        for (std::uint64_t control : control_qubits) {
+            filled_step[control] = max_step_amount_target_qubits + 1;
+        }
+        for (std::uint64_t target : target_qubits) {
+            filled_step[target] = max_step_amount_target_qubits + 1;
+        }
+    }
+    return *std::ranges::max_element(filled_step);
+}
+
+FLOAT(Fp)
+void Circuit<Fp>::add_circuit(const Circuit<Fp>& circuit) {
+    if (circuit._n_qubits != _n_qubits) {
+        throw std::runtime_error(
+            "Circuit::add_circuit(const Circuit&): circuit with different qubit count cannot "
+            "be merged.");
+    }
+    _gate_list.reserve(_gate_list.size() + circuit._gate_list.size());
+    for (const auto& gate : circuit._gate_list) {
+        _gate_list.push_back(gate);
+    }
+}
+FLOAT(Fp)
+void Circuit<Fp>::add_circuit(Circuit<Fp>&& circuit) {
+    if (circuit._n_qubits != _n_qubits) {
+        throw std::runtime_error(
+            "Circuit::add_circuit(Circuit&&): circuit with different qubit count cannot be "
+            "merged.");
+    }
+    _gate_list.reserve(_gate_list.size() + circuit._gate_list.size());
+    for (auto&& gate : circuit._gate_list) {
+        _gate_list.push_back(std::move(gate));
+    }
+}
+
+FLOAT(Fp)
+void Circuit<Fp>::update_quantum_state(StateVector<Fp>& state,
+                                       const std::map<std::string, Fp>& parameters) const {
+    for (auto&& gate : _gate_list) {
+        if (gate.index() == 0) continue;
+        const auto& key = std::get<1>(gate).second;
+        if (!parameters.contains(key)) {
+            using namespace std::string_literals;
+            throw std::runtime_error(
+                "Circuit::update_quantum_state(StateVector&, const std::map<std::string_view, double>&) const: parameter named "s +
+                std::string(key) + "is not given.");
+        }
+    }
+    for (auto&& gate : _gate_list) {
+        if (gate.index() == 0) {
+            std::get<0>(gate)->update_quantum_state(state);
+        } else {
+            const auto& [param_gate, key] = std::get<1>(gate);
+            param_gate->update_quantum_state(state, parameters.at(key));
+        }
+    }
+}
+
+FLOAT(Fp)
+Circuit<Fp> Circuit<Fp>::copy() const {
+    Circuit ccircuit(_n_qubits);
+    ccircuit._gate_list.reserve(_gate_list.size());
+    for (auto&& gate : _gate_list) {
+        if (gate.index() == 0)
+            ccircuit._gate_list.push_back(std::get<0>(gate));
+        else {
+            const auto& [param_gate, key] = std::get<1>(gate);
+            ccircuit._gate_list.push_back(std::make_pair(param_gate, key));
+        }
+    }
+    return ccircuit;
+}
+
+FLOAT(Fp)
+Circuit<Fp> Circuit<Fp>::get_inverse() const {
+    Circuit icircuit(_n_qubits);
+    icircuit._gate_list.reserve(_gate_list.size());
+    for (auto&& gate : _gate_list | std::views::reverse) {
+        if (gate.index() == 0)
+            icircuit._gate_list.push_back(std::get<0>(gate)->get_inverse());
+        else {
+            const auto& [param_gate, key] = std::get<1>(gate);
+            icircuit._gate_list.push_back(std::make_pair(param_gate->get_inverse(), key));
+        }
+    }
+    return icircuit;
+}
+
+FLOAT(Fp)
+void Circuit<Fp>::check_gate_is_valid(const Gate<Fp>& gate) const {
+    auto targets = gate->target_qubit_list();
+    auto controls = gate->control_qubit_list();
+    bool valid = true;
+    if (!targets.empty()) valid &= *std::max_element(targets.begin(), targets.end()) < _n_qubits;
+    if (!controls.empty()) valid &= *std::max_element(controls.begin(), controls.end()) < _n_qubits;
+    if (!valid) {
+        throw std::runtime_error("Gate to be added to the circuit has invalid qubit range");
+    }
+}
+
+FLOAT(Fp)
+void Circuit<Fp>::check_gate_is_valid(const ParamGate<Fp>& gate) const {
+    auto targets = gate->target_qubit_list();
+    auto controls = gate->control_qubit_list();
+    bool valid = true;
+    if (!targets.empty()) valid &= *std::max_element(targets.begin(), targets.end()) < _n_qubits;
+    if (!controls.empty()) valid &= *std::max_element(controls.begin(), controls.end()) < _n_qubits;
+    if (!valid) {
+        throw std::runtime_error("Gate to be added to the circuit has invalid qubit range");
+    }
+}
+}  // namespace scaluq
