681414a2d9afbf20c1158f9e26c0bcba518c5dec
KowerKoint <KowerKoint>
2025-02-07 12:02:02 +0900

    fix example_project

--- example_project/main.cpp
+++ example_project/main.cpp
@@ -1,9 +1,10 @@
 #include <scaluq/state/state_vector.hpp>
 
 int main() {
+    constexpr scaluq::Precision Prec = scaluq::Precision::F64;
     scaluq::initialize();
     {
-        scaluq::StateVector<double> state(2);
+        scaluq::StateVector<Prec> state(2);
         std::cout << state << std::endl;
     }
     scaluq::finalize();
