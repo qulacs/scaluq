6797d114bd9a75243b56b062a2cca587151a0801
gandalfr-KY <oukanimiso@gmail.com>
2025-04-01 01:06:01 +0000

    changed namespace

--- include/scaluq/types.hpp
+++ include/scaluq/types.hpp
@@ -79,7 +79,7 @@ struct adl_serializer<::scaluq::StdComplex> {
 
 #ifdef SCALUQ_USE_NANOBIND
 #include <nanobind/nanobind.h>
-namespace scaluq {
+namespace scaluq::internal {
 namespace nb = nanobind;
 }
 #endif
