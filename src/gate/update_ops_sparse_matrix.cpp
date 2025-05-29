67c027fdc0afc48c92016ee40f3f6fe41c035917
gandalfr-KY <oukanimiso@gmail.com>
2025-05-29 05:47:34 +0000

    fix sparse gate

--- src/gate/update_ops_sparse_matrix.cpp
+++ src/gate/update_ops_sparse_matrix.cpp
@@ -24,21 +24,31 @@ void sparse_matrix_gate(std::uint64_t target_mask,
 
     std::uint64_t outer_mask = ~target_mask & ((1ULL << state.n_qubits()) - 1);
     Kokkos::parallel_for(
-        "COO_Update",
-        Kokkos::MDRangePolicy<SpaceType<Space>, Kokkos::Rank<2>>(
-            {0, 0},
-            {static_cast<std::int64_t>(state.dim() >> std::popcount(target_mask | control_mask)),
-             static_cast<std::int64_t>(values.size())}),
-        KOKKOS_LAMBDA(std::uint64_t outer, std::uint64_t inner) {
-            std::uint64_t basis =
-                internal::insert_zero_at_mask_positions(outer, target_mask | control_mask) |
-                control_value_mask;
-            auto [v, r, c] = values(inner);
-            std::uint32_t src_index =
-                internal::insert_zero_at_mask_positions(c, outer_mask) | basis;
-            std::uint32_t dst_index =
-                internal::insert_zero_at_mask_positions(r, outer_mask) | basis;
-            update_atomic(dst_index) += v * state._raw(src_index);
+        "sparse_matrix_gate (update)",
+        Kokkos::TeamPolicy<SpaceType<Space>>(
+            SpaceType<Space>(),
+            state.dim() >> std::popcount(target_mask | control_mask),
+            Kokkos::AUTO),
+        KOKKOS_LAMBDA(const Kokkos::TeamPolicy<SpaceType<Space>>::member_type& team) {
+            std::uint64_t basis = team.league_rank();
+            basis = insert_zero_at_mask_positions(basis, target_mask | control_mask) |
+                    control_value_mask;
+            Kokkos::parallel_for(Kokkos::TeamThreadRange(team, mat._rows), [&](std::uint64_t r) {
+                std::uint64_t dst_index =
+                    internal::insert_zero_at_mask_positions(r, outer_mask) | basis;
+                Complex<Prec> sum = Float<Prec>{0};
+                Kokkos::parallel_reduce(
+                    Kokkos::ThreadVectorRange(team, mat._row_ptr[r], mat._row_ptr[r + 1]),
+                    [&](std::uint64_t idx, Complex<Prec>& inner_sum) {
+                        std::uint64_t c = mat._col_idx[idx];
+                        std::uint64_t src_index =
+                            internal::insert_zero_at_mask_positions(c, outer_mask) | basis;
+                        inner_sum += mat._vals[idx] * state._raw(src_index);
+                    },
+                    sum);
+                update(dst_index) = sum;
+            });
+            team.team_barrier();
         });
     Kokkos::fence();
 
@@ -70,20 +80,30 @@ void sparse_matrix_gate(std::uint64_t target_mask,
     std::uint64_t outer_mask = ~target_mask & ((1ULL << states.n_qubits()) - 1);
     std::uint64_t outer_size = states.dim() >> std::popcount(target_mask | control_mask);
     Kokkos::parallel_for(
-        "COO_Update",
-        Kokkos::MDRangePolicy<SpaceType<Space>, Kokkos::Rank<3>>(
-            {0, 0, 0},
-            {static_cast<std::int64_t>(states.batch_size()),
-             static_cast<std::int64_t>(states.dim() >> std::popcount(target_mask | control_mask)),
-             static_cast<std::int64_t>(values.size())}),
-        KOKKOS_LAMBDA(std::uint64_t batch_id, std::uint64_t outer, std::uint64_t inner) {
-            std::uint64_t basis =
-                internal::insert_zero_at_mask_positions(outer, target_mask | control_mask) |
-                control_value_mask;
-            auto [v, r, c] = values(inner);
-            uint32_t src_index = internal::insert_zero_at_mask_positions(c, outer_mask) | basis;
-            uint32_t dst_index = internal::insert_zero_at_mask_positions(r, outer_mask) | basis;
-            update_atomic(batch_id, dst_index) += v * states._raw(batch_id, src_index);
+        "sparse_matrix_gate (update)",
+        Kokkos::TeamPolicy<SpaceType<Space>>(
+            SpaceType<Space>(), outer_size * states.batch_size(), Kokkos::AUTO),
+        KOKKOS_LAMBDA(const Kokkos::TeamPolicy<SpaceType<Space>>::member_type& team) {
+            std::uint64_t basis = team.league_rank() % outer_size;
+            std::uint64_t batch_id = team.league_rank() / outer_size;
+            basis = insert_zero_at_mask_positions(basis, target_mask | control_mask) |
+                    control_value_mask;
+            Kokkos::parallel_for(Kokkos::TeamThreadRange(team, mat._rows), [&](std::uint64_t r) {
+                std::uint64_t dst_index =
+                    internal::insert_zero_at_mask_positions(r, outer_mask) | basis;
+                Complex<Prec> sum = Float<Prec>{0};
+                Kokkos::parallel_reduce(
+                    Kokkos::ThreadVectorRange(team, mat._row_ptr[r], mat._row_ptr[r + 1]),
+                    [&](std::uint64_t idx, Complex<Prec>& inner_sum) {
+                        std::uint64_t c = mat._col_idx[idx];
+                        std::uint64_t src_index =
+                            internal::insert_zero_at_mask_positions(c, outer_mask) | basis;
+                        inner_sum += mat._vals[idx] * states._raw(batch_id, src_index);
+                    },
+                    sum);
+                update(batch_id, dst_index) = sum;
+            });
+            team.team_barrier();
         });
     Kokkos::fence();
 
