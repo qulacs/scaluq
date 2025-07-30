9eadc8b8433efdf2fdcc28bf4ada01c3ce528dab
gandalfr-KY <oukanimiso@gmail.com>
2025-07-30 18:13:55 +0000

    revert main

--- exe/main.cpp
+++ exe/main.cpp
@@ -7,17 +7,26 @@ using namespace nlohmann;
 
 template <Precision Prec, ExecutionSpace Space>
 void merge_gate_test() {
-    const std::uint64_t n_qubits = 3;
-    StateVector<Prec, Space> state_vector(n_qubits);
-    state_vector.load([n_qubits] {
-        std::vector<StdComplex> tmp(1 << n_qubits);
-        for (std::uint64_t i = 0; i < tmp.size(); ++i) tmp[i] = StdComplex(i, 0);
-        return tmp;
-    }());
+    std::uint64_t n = 4;
+    Random random;
 
-    // PauliOperator<Prec, Space> op(0b001, 0b010, StdComplex(2));
-    // op.apply_to_state(state_vector);
-    // std::vector<StdComplex> expected = {2, 0, -6, -4, 10, 8, -14, -12};
+    for (std::uint64_t repeat = 0; repeat < 1; ++repeat) {
+        auto op1 = generate_random_observable_with_eigen<Prec, Space>(n, random).first;
+        auto op2 = generate_random_observable_with_eigen<Prec, Space>(n, random).first;
+        auto op = op1 + op2;
+        std::cout << "Operator 1: " << op1.to_string() << std::endl;
+        std::cout << "Operator 2: " << op2.to_string() << std::endl;
+        std::cout << "Operator: " << op.to_string() << std::endl;
+        std::cout << "Operator minus: " << (-op1).to_string() << std::endl;
+        auto state = StateVector<Prec, Space>::Haar_random_state(n);
+        auto exp1 = op1.get_expectation_value(state);
+        auto exp2 = op2.get_expectation_value(state);
+        auto exp = op.get_expectation_value(state);
+        std::cout << "Expectation value: " << exp << std::endl;
+        std::cout << "Expectation value 1: " << exp1 << std::endl;
+        std::cout << "Expectation value 2: " << exp2 << std::endl;
+        std::cout << "Difference: " << std::abs(exp1 + exp2 - exp) << std::endl;
+    }
 }
 
 int main() {
