213639849c1bca25640c0a60aa01d8ee6c6cd7b1
KowerKoint <KowerKoint>
2025-10-28 03:06:32 +0000

    add about merge_gate

--- doc/source/tutorials/python/gate.md
+++ doc/source/tutorials/python/gate.md
@@ -136,3 +136,24 @@ print(state.get_amplitudes()) # [(0.7071067811865476+0j), (0.7071067811865476+0j
 cx01.update_quantum_state(state)
 print(state.get_amplitudes()) # [(0.7071067811865476+0j), 0j, 0j, (0.7071067811865476+0j)]
 ```
+
+## Merge two Gates
+You can merge two Gates by {func}`merge_gate <scaluq.default.f64.gate.merge_gate>`
+The type of result gate is flexible.
+
+```py
+from scaluq.default.f64.gate import X, Y, RX, RY, RZ, S
+from scaluq.default.f64 import merge_gate
+import math
+
+def print_merge_result(gate1, gate2):
+    mgate, phase = merge_gate(gate1, gate2)
+    print("Gate:")
+    print(mgate.to_string())
+    print("Phase:", phase)
+
+print_merge_result(X(0), X(0)) # Gate=I(), Phase=0
+print_merge_result(X(0), Y(0)) # Gate=Z(0), Phase=-pi/2
+print_merge_result(RZ(0, -math.pi/8*3), S(0)) # Gate=U1(0, math.pi/8), Phase=math.pi/16*3)
+print_merge_result(RX(2, math.pi/6, controls=[1]), RY(2, math.pi/3, controls=[0, 1])) # gate=DenseMatrix([0,2], [...], controls=[1], Phase=0)
+```
--- exe/main.py
+++ exe/main.py
@@ -1,11 +1,14 @@
-from scaluq.default.f64 import StateVector
-from scaluq.default.f64.gate import H, CX
+from scaluq.default.f64.gate import X, Y, RX, RY, RZ, S
+from scaluq.default.f64 import merge_gate
+import math
 
-h0 = H(0)
-cx01 = CX(0, 1)
-state = StateVector(2)
-print(state.get_amplitudes()) # [(1+0j), 0j, 0j, 0j]
-h0.update_quantum_state(state)
-print(state.get_amplitudes()) # [(0.7071067811865476+0j), (0.7071067811865476+0j), 0j, 0j]
-cx01.update_quantum_state(state)
-print(state.get_amplitudes()) # [(0.7071067811865476+0j), 0j, 0j, (0.7071067811865476+0j)]
+def print_merge_result(gate1, gate2):
+    mgate, phase = merge_gate(gate1, gate2)
+    print("Gate:")
+    print(mgate.to_string())
+    print("Phase:", phase)
+
+print_merge_result(X(0), X(0)) # Gate=I(), Phase=0
+print_merge_result(X(0), Y(0)) # Gate=Z(0), Phase=-pi/2
+print_merge_result(RZ(0, -math.pi/8*3), S(0)) # Gate=U1(0, math.pi/8), Phase=math.pi/16*3)
+print_merge_result(RX(2, math.pi/6, controls=[1]), RY(2, math.pi/3, controls=[0, 1])) # gate=DenseMatrix([0,2], [...], controls=[1], Phase=0)
