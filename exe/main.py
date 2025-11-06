7a4963bf292ce635528a333a9e718d66472157b2
gandalfr-KY <oukanimiso@gmail.com>
2025-11-06 10:53:37 +0000

    bind ok

--- exe/main.py
+++ exe/main.py
@@ -6,4 +6,7 @@ op1 = Operator(paulis1)
 paulis2 = [PauliOperator("X 1", -1j)]
 op2 = Operator(paulis2)
 op = op1 * op2
-print(op.to_json())
+op_batched = OperatorBatched([op1, op2])
+print(op_batched)
+op_batched = op_batched * [-1j, 2.0]
+print(op_batched)
--- include/scaluq/operator/operator_batched.hpp
+++ include/scaluq/operator/operator_batched.hpp
@@ -87,19 +87,20 @@ public:
     OperatorBatched& operator*=(const std::vector<StdComplex>& coef);
     OperatorBatched operator+() const { return *this; }
     OperatorBatched operator-() const {
-        return *this * std::vector<StdComplex>(StdComplex(_row_ptr.extent(0) - 1, -1.));
+        OperatorBatched result = copy();
+        Kokkos::parallel_for(
+            "OperatorBatched::operator-",
+            Kokkos::RangePolicy<ExecutionSpaceType>(0, _ops.extent(0)),
+            KOKKOS_LAMBDA(const std::uint64_t i) { result._ops(i) *= -1.; });
+        return result;
     }
     OperatorBatched operator+(const OperatorBatched& target) const;
-    OperatorBatched operator-(const OperatorBatched& target) const {
-        return *this + target * std::vector<StdComplex>(StdComplex(_row_ptr.extent(0) - 1, -1.));
-    }
+    OperatorBatched operator-(const OperatorBatched& target) const { return *this + (-target); }
     OperatorBatched operator*(const OperatorBatched& target) const;
     OperatorBatched operator+(const std::vector<PauliOperator<Prec, Space>>& pauli) const;
     OperatorBatched operator-(const std::vector<PauliOperator<Prec, Space>>& pauli) const {
         auto m_pauli = pauli;
-        for (auto& p : m_pauli) {
-            p *= -1.;
-        }
+        for (auto& p : m_pauli) p *= -1.;
         return *this + m_pauli;
     }
     OperatorBatched operator*(const std::vector<PauliOperator<Prec, Space>>& pauli) const;
@@ -190,9 +191,9 @@ void bind_operator_operator_batched_hpp(nb::module_& m) {
                  .build_as_google_style()
                  .c_str())
         .def("get_applied_states",
+             &OperatorBatched<Prec, Space>::get_applied_states,
              "state_vector"_a,
              "batch_size"_a = 1,
-             &OperatorBatched<Prec, Space>::get_applied_states,
              DocString()
                  .desc("Apply the batched operator to a state vector.")
                  .arg("state_vector", "A state vector to be applied.")
@@ -218,30 +219,30 @@ void bind_operator_operator_batched_hpp(nb::module_& m) {
                  .arg("state_vector_ket", "A ket state vector.")
                  .build_as_google_style()
                  .c_str())
-        .def("solve_ground_state_eigenvalue_by_arnoldi_method",
-             &OperatorBatched<Prec, Space>::solve_ground_state_eigenvalue_by_arnoldi_method,
-             "state"_a,
-             "iter_count"_a,
-             "mu"_a = std::nullopt,
-             DocString()
-                 .desc("Solve the ground state eigenvalue using the Arnoldi method.")
-                 .arg("state", "An initial state vector.")
-                 .arg("iter_count", "Number of iterations.")
-                 .arg("mu", "A shift parameter.")
-                 .build_as_google_style()
-                 .c_str())
-        .def("solve_ground_state_eigenvalue_by_power_method",
-             &OperatorBatched<Prec, Space>::solve_ground_state_eigenvalue_by_power_method,
-             "state"_a,
-             "iter_count"_a,
-             "mu"_a = std::nullopt,
-             DocString()
-                 .desc("Solve the ground state eigenvalue using the Power method.")
-                 .arg("state", "An initial state vector.")
-                 .arg("iter_count", "Number of iterations.")
-                 .arg("mu", "A shift parameter.")
-                 .build_as_google_style()
-                 .c_str())
+        // .def("solve_ground_state_eigenvalue_by_arnoldi_method",
+        //      &OperatorBatched<Prec, Space>::solve_ground_state_eigenvalue_by_arnoldi_method,
+        //      "state"_a,
+        //      "iter_count"_a,
+        //      "mu"_a = std::nullopt,
+        //      DocString()
+        //          .desc("Solve the ground state eigenvalue using the Arnoldi method.")
+        //          .arg("state", "An initial state vector.")
+        //          .arg("iter_count", "Number of iterations.")
+        //          .arg("mu", "A shift parameter.")
+        //          .build_as_google_style()
+        //          .c_str())
+        // .def("solve_ground_state_eigenvalue_by_power_method",
+        //      &OperatorBatched<Prec, Space>::solve_ground_state_eigenvalue_by_power_method,
+        //      "state"_a,
+        //      "iter_count"_a,
+        //      "mu"_a = std::nullopt,
+        //      DocString()
+        //          .desc("Solve the ground state eigenvalue using the Power method.")
+        //          .arg("state", "An initial state vector.")
+        //          .arg("iter_count", "Number of iterations.")
+        //          .arg("mu", "A shift parameter.")
+        //          .build_as_google_style()
+        //          .c_str())
         .def("get_operator_at",
              &OperatorBatched<Prec, Space>::get_operator_at,
              "index"_a,
@@ -261,7 +262,31 @@ void bind_operator_operator_batched_hpp(nb::module_& m) {
         .def(nb::self + std::vector<PauliOperator<Prec, Space>>())
         .def(nb::self - std::vector<PauliOperator<Prec, Space>>())
         .def(nb::self * std::vector<PauliOperator<Prec, Space>>())
-        .def(nb::self *= std::vector<PauliOperator<Prec, Space>>());
+        .def(nb::self *= std::vector<PauliOperator<Prec, Space>>())
+        .def(
+            "to_json",
+            [](const OperatorBatched<Prec, Space>& op) { return Json(op).dump(); },
+            "Information as json style.")
+        .def(
+            "load_json",
+            [](OperatorBatched<Prec, Space>& op, const std::string& str) {
+                Json j = Json::parse(str);
+                op = j;
+            },
+            "json_str"_a,
+            "Read an object from the JSON representation of the OperatorBatched.")
+        .def("to_string",
+             &OperatorBatched<Prec, Space>::to_string,
+             DocString()
+                 .desc("Get string representation of the OperatorBatched.")
+                 .build_as_google_style()
+                 .c_str())
+        .def("__str__",
+             &OperatorBatched<Prec, Space>::to_string,
+             DocString()
+                 .desc("Get string representation of the OperatorBatched.")
+                 .build_as_google_style()
+                 .c_str());
 }
 }  // namespace internal
 #endif
