6a8c6058c6c9d44e1b2d48289ed58f94374aca2e
gandalfr-KY <oukanimiso@gmail.com>
2026-02-05 07:49:43 +0000

    add include to all.hpp

--- include/scaluq/all.hpp
+++ include/scaluq/all.hpp
@@ -7,6 +7,7 @@
 #include "gate/merge_gate.hpp"
 #include "gate/param_gate.hpp"
 #include "gate/param_gate_factory.hpp"
+#include "kokkos.hpp"
 #include "operator/operator.hpp"
 #include "operator/operator_batched.hpp"
 #include "operator/pauli_operator.hpp"
