afb2c8d2f5355447a53638a4ea11bd4ecc81461c
KowerKoint <kowerkoint@gmail.com>
2026-05-12 12:32:06 +0900

    prec-space-free bindings

--- python/scaluq/host/f16/__init__.py
+++
@@ -1,6 +0,0 @@
-import scaluq as _scaluq
-if _scaluq.get_default_execution_space() == 'cuda':
-    from ...scaluq_core.host.f16 import *
-else:
-    from ...scaluq_core.default.f16 import *
-from . import gate
--- python/scaluq/host/f32/gate/__init__.py
+++
@@ -1,5 +0,0 @@
-import scaluq as _scaluq
-if _scaluq.get_default_execution_space() == 'cuda':
-    from ....scaluq_core.host.f32.gate import *
-else:
-    from ....scaluq_core.default.f32.gate import *
